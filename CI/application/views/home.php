b343d7686948b2f77901c4f8a9a61a3ef7d50f6c
Rochmatiningsih26 <66780941+Rochmatiningsih26@users.noreply.github.com>
2021-03-18 08:56:01 +0700

    rochmatiningsih

--- CI/application/views/home.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Rochmatiningsih</title>
-</head>
-<body>
-<h2>Rochmatiningsih E31191159 MIF-B</h2>
-</body>
-</html>
--- Minggu_6_WEB_Framework/ci/application/controllers/Hallo.php
+++ Minggu_6_WEB_Framework/ci/application/controllers/Hallo.php
@@ -1,7 +1,7 @@
-<?php 
+  
+<?php
 class Hello extends CI_Controller{
 	public function index(){
-		$this->load->view('helloview');  //file view
+		echo "<h2>Hello World CI!</h2>";
 	}
 }
- ?>
